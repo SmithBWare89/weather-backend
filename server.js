d65eded78acff290f41a7f23c6d35530db72838c
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-26 16:13:57 -0500

    Testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./api");
-const PORT = process.env.API_URL || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
