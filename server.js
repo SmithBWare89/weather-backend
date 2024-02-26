51eeb384447177208f2ad32163db812b85baa628
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-26 16:04:06 -0500

    Updates

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./api");
-const PORT = process.env.PORT;
+const PORT = process.env.API_URL || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on PORT ${PORT}`);
-});
+app.listen(PORT, () => {});
