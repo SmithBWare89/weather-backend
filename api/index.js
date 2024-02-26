ee2de5a9ddb4097d3fb3da0bcea7e80a5c9a0fa4
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-26 16:25:42 -0500

    Updated app

--- api/index.js
+++ api/index.js
@@ -5,10 +5,12 @@ router.get("/", (req, res) => {
 });
 router.get("/:city", async (req, res) => {
   try {
+    const url = `${process.env.API_URL}${req.params.city}&appId=${process.env.API_KEY}`;
+    console.log("URL: ", url);
     const fetchCity = await fetch(
       `${process.env.API_URL}${req.params.city}&appId=${process.env.API_KEY}`,
     );
-    const formattedResponse = await rawResponse.json();
+    const formattedResponse = await fetchCity.json();
 
     if (formattedResponse?.cod === "404") {
       res.status(404).send("Unable to find city.");
