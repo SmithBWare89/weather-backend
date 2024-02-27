6aa09d9e633fa2de8982edaa928da8b08fc1422b
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-27 18:05:26 -0500

    Added formatting to the response being sent. Added cors rule to only allow certain domains to send api requests.

--- api/api.utility.js
+++ api/api.utility.js
@@ -7,3 +7,51 @@ export const forecastUrlBuilder = (formattedCityResponse) => {
   const cityLon = formattedCityResponse.coord.lon;
   return `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,minutely&units=imperial&appid=${process.env.API_KEY}&q`;
 };
+
+export const toResponseType = (forecastResponse) => {
+  const { current, daily, lat, lon } = forecastResponse;
+  return {
+    currentForecast: {
+      dewPoint: current.dew_point,
+      cloudCover: current.clouds,
+      feelsLike: current.feels_like,
+      sunrise: current.sunrise,
+      sunset: current.sunset,
+      temp: current.temp,
+      uvi: current.uvi,
+      windSpeed: current.wind_speed,
+      date: current.dt,
+      weather: formatWeather(current.weather),
+    },
+    fiveDayForecast: formatForecast(daily),
+    latitude: lat,
+    longitude: lon,
+  };
+};
+
+const formatForecast = (dailyForecast) => {
+  return dailyForecast.map((forecast) => {
+    return {
+      dewPoint: forecast.dew_point,
+      cloudCover: forecast.clouds,
+      feelsLike: forecast.feels_like.day,
+      sunrise: forecast.sunrise,
+      sunset: forecast.sunset,
+      uvi: forecast.uvi,
+      weather: formatWeather(forecast.weather),
+      windSpeed: forecast.wind_speed,
+      date: forecast.dt,
+    };
+  });
+};
+
+const formatWeather = (weather) => {
+  return weather.map((weather) => {
+    return {
+      description: weather.description,
+      icon: weather.icon,
+      iconId: weather.id,
+      main: weather.main,
+    };
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,25 +1,33 @@
 import express from "express";
 const router = express.Router();
-import { cityUrlBuilder, forecastUrlBuilder } from "./api.utility.js";
+import {
+  cityUrlBuilder,
+  forecastUrlBuilder,
+  toResponseType,
+} from "./api.utility.js";
 
-router.get("/", (req, res) => {
-  res.send(`<h1>Hello!</h1>`);
-});
 router.get("/:city", async (req, res) => {
   try {
     const cityUrl = cityUrlBuilder(req.params.city);
-    console.log(cityUrl);
     const fetchCity = await fetch(cityUrl);
     const formattedCityResponse = await fetchCity.json();
-    console.log(formattedCityResponse);
-
+    const origin = req.get("origin");
+    const cors =
+      origin === process.env.PROD
+        ? process.env.PROD
+        : origin === process.env.DEV
+          ? process.env.DEV
+          : undefined;
+    if (!cors) {
+      return res.status(403).send("Forbidden");
+    }
+    res.set("Access-Control-Allow-Origin", cors);
     const forecastUrl = forecastUrlBuilder(formattedCityResponse);
     const fetchForecast = await fetch(forecastUrl);
     const formattedForecastResponse = await fetchForecast.json();
-    res.set("Access-Control-Allow-Origin", process.env.PROD || process.env.DEV);
-    res.send(formattedForecastResponse);
+    res.status(200).send(toResponseType(formattedForecastResponse));
   } catch (err) {
-    res.status(404).send(`The following error has occurred ${err}`);
+    res.status(404).send(`The following error has occurred: ${err}`);
   }
 });
 
